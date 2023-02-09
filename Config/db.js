e028eaf35f819d33ca128a5e4433e3b231454b7e
Siddhi Dhamnaskar <siddhidhamnaskar64@gmail.com>
2023-02-09 08:22:44 +0530

    done2

--- Config/db.js
+++ Config/db.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 mongoose.set('strictQuery',true);
 
 const connection=async()=>{
-    await mongoose.connect("mongodb+srv://siddhidhamnaskar:Siddhi@2000@cluster0.klwghfz.mongodb.net/insta?retryWrites=true&w=majority");
+    await mongoose.connect("mongodb+srv://zoomcar:zoomcar_123@cluster0.kokkwza.mongodb.net/?retryWrites=true&w=majority");
 }
 
 module.exports=connection;
