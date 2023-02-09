29b55c023cd93c788eca51c914c836e8b79ca9ac
Siddhi Dhamnaskar <siddhidhamnaskar64@gmail.com>
2023-02-09 06:44:14 +0530

    done

--- middlewares/authinticator.js
+++ middlewares/authinticator.js
@@ -2,12 +2,13 @@
  const jwt=require("jsonwebtoken");
 
  require("dotenv").config();
+ const JWT_SECRET_KEY=process.env.JWT_SECRET_KEY;
 
  const authinticator=(req,res,next)=>{
     let token=req.header.authorisation;
     if(token)
     {
-        const decoded=jwt.verify(token,process.env.key);
+        const decoded=jwt.verify(token,JWT_SECRET_KEY);
         if(decoded)
         {
             let userId=decoded.userId;
